3243cc1d14f4c1ad26c158ee1125b06fa161f7cb
Will Paul <wpaul@globalthinking.com>
2014-05-07 13:30:03 -0400

    works in firefox, fails in safari

--- lib/rulers/controller.rb
+++ lib/rulers/controller.rb
@@ -12,12 +12,13 @@ module Rulers
     end
 
     def render view_name, locals = {}
-      filename = File.join "app", "view", "#{view_name}.html.slim"
-      template = File.read filename
-
-      Slim::Template.new(template).render(locals.merge env: env)
-
-      # Slim::Template.new("app/views/#{view_name}.html.slim").render(locals.merge env: env)
+      filename = File.join "app", "views", "#{view_name}.html.slim"
+      
+      puts File.expand_path filename
+      puts File.file? filename
+      puts Slim::Template.new(filename).render(env: env)
+      
+      Slim::Template.new(filename).render(env: env)
     end
   end
 end
